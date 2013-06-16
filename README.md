e9045fcf49cbeb9e936b78ea826b0c55f99aa778
Aristide Niyungeko <niyungeko@gmail.com>
2013-06-16 12:05:55 -0700

    1. Add a CDT timeline data parser 2. Ignore IDE files

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ CMUSV_SE_Practicum_2013
 # Tests
 
 Tests are grouped by weekly topics:
-	Tests written for one weekly topic are in the directory "tests/week#", i.e "tests/week1" for week 1 tests.
+	Tests written for one weekly topic are in the directory `tests/week#`, i.e `tests/week1` for week 1 tests.
+	
+Tests are deployed [here](http://ec2-54-215-135-205.us-west-1.compute.amazonaws.com/se_practicum_13/).
---
+++ test_harness/timeline_parser/src/frame_timer.py
@@ -0,0 +1,86 @@
+__author__ = 'Aristide'
+
+import sys
+import json
+import os
+
+def deserialize_timeline_capture(path):
+    json_file = open(path, 'r')
+    deser_json = json.load(json_file)
+    return deser_json
+
+def check_for_event_type (list_of_objects, event_name):
+    event_is_present = False
+    i = 0
+    list_length = len(list_of_objects)
+
+    while (event_is_present != True and i < list_length):
+        obj = list_of_objects[i]
+        if event_name in obj.values():
+            event_is_present = True
+        else:
+            i += 1
+
+    return event_is_present
+
+def get_frame_timings(events_data, event_before_frame):
+    is_frame_event_pending = False
+    timings = []
+
+    for object in events_data:
+        # The true events list is the val of the "children key"
+        events_list = object["children"]
+
+        if (is_frame_event_pending):
+            # Check for the beginFrame event; when found:
+            # 1. push the timing of the object in the list of timings,
+            # 2. toggle the frame_event_pending flag
+            is_frame_event_found = check_for_event_type(events_list, "BeginFrame")
+
+            if (is_frame_event_found == True):
+                timings.append(object["endTime"] - object["startTime"])
+                is_frame_event_pending = False
+
+        else:
+            # Check for event_before_frame in the list and toggle the frame_event_pending flag
+            is_event_before_frame_found = check_for_event_type(events_list, event_before_frame)
+
+            if (is_event_before_frame_found == True):
+                is_frame_event_pending = True
+
+    return timings
+
+
+def main(argv):
+    #pass event name markLoad to func
+    #print list returned
+    #
+
+    # Get timeline data
+    file_path = os.path.abspath('../timeline_data4.json')
+    timeline_data = deserialize_timeline_capture(file_path)
+    environment_data = timeline_data[0]
+    events_data = timeline_data[1:]
+
+    # print timeline_data
+    print environment_data
+    # print json.dumps(events_data, indent=4, separators=(',', ': '))
+
+    # Get frame timings after load, and print 'em
+    load_event_type = "MarkLoad"
+    next_frame_timings = get_frame_timings(events_data, load_event_type)
+    print next_frame_timings
+
+    # Get frame timings after style change, and print 'em
+    style_change_event_type = "RecalculateStyles"
+    next_frame_timings = get_frame_timings(events_data, style_change_event_type)
+    print next_frame_timings
+
+    # Get frame timings after style change
+    # style_change_event_type = ""
+
+
+if __name__ == '__main__':
+    main()
+
+
